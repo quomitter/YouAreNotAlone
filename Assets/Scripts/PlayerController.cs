d24b92e0ee5e4a41a76ecb3cef9c96fdb49e2504
quomitter <109629602+quomitter@users.noreply.github.com>
2022-08-21 08:08:03 -0400

    changes

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using Unity.VisualScripting;
 
 public class PlayerController : MonoBehaviour
 {
@@ -12,6 +13,32 @@ public class PlayerController : MonoBehaviour
     public AudioClip aJump;
     AudioSource audioSource;
 
+    [SerializeField] Rigidbody2D playerRb;
+    [SerializeField] Rigidbody2D buddy1;
+    [SerializeField] Rigidbody2D buddy2;
+    [SerializeField] Rigidbody2D buddy3;
+    [SerializeField] Rigidbody2D buddy4;
+    [SerializeField] Rigidbody2D buddy5;
+    [SerializeField] Rigidbody2D buddy6;
+
+    [SerializeField] Vector2 playerLastPosition;
+    [SerializeField] Vector2 Buddy1LastPosition;
+    [SerializeField] Vector2 Buddy2LastPosition;
+    [SerializeField] Vector2 Buddy3LastPosition;
+    [SerializeField] Vector2 Buddy4LastPosition;
+    [SerializeField] Vector2 Buddy5LastPosition;
+    [SerializeField] Vector2 Buddy6LastPosition;
+
+    float distanceFromPlayer;
+    float distanceFromBuddy1;
+    float distanceFromBuddy2;
+    float distanceFromBuddy3;
+    float distanceFromBuddy4;
+    float distanceFromBuddy5;
+    float distanceFromBuddy6;
+
+    float step;
+
     public float runSpeed = 40f;
     public float bounceForce;
 
@@ -19,7 +46,7 @@ public class PlayerController : MonoBehaviour
     bool jump = false;
     //bool crouch = false;
 
-    private Rigidbody2D rb;
+   
     public float dashspeed;
     private float dashTime;
     public float startDashTime;
@@ -29,7 +56,17 @@ public class PlayerController : MonoBehaviour
     private void Start()
     {
         audioSource = GetComponent<AudioSource>();
-        rb = GetComponent<Rigidbody2D>();
+
+
+        playerRb = GetComponent<Rigidbody2D>();
+        buddy1 = GameObject.FindGameObjectWithTag("Buddy1").GetComponent<Rigidbody2D>();
+        buddy2 = GameObject.FindGameObjectWithTag("Buddy2").GetComponent<Rigidbody2D>();
+        buddy3 = GameObject.FindGameObjectWithTag("Buddy3").GetComponent<Rigidbody2D>();
+        buddy4 = GameObject.FindGameObjectWithTag("Buddy4").GetComponent<Rigidbody2D>();
+        buddy5 = GameObject.FindGameObjectWithTag("Buddy5").GetComponent<Rigidbody2D>();
+        buddy6 = GameObject.FindGameObjectWithTag("Buddy6").GetComponent<Rigidbody2D>();
+
+
         dashTime = startDashTime;
         isDashing = false;
         counterDash = 0;
@@ -37,6 +74,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        step = 10f * Time.deltaTime;
 
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
 
@@ -70,10 +108,85 @@ public class PlayerController : MonoBehaviour
             {
                 dashTime -= Time.deltaTime;
        
-                rb.velocity = new Vector2(dashspeed * horizontalMove, rb.velocity.y);
+                playerRb.velocity = new Vector2(dashspeed * horizontalMove, playerRb.velocity.y);
 
             }
         }
+
+        //FollowCode
+
+        distanceFromPlayer = Vector2.Distance(playerRb.position, buddy1.position);
+        distanceFromBuddy1 = Vector2.Distance(buddy1.position, buddy2.position);
+        distanceFromBuddy2 = Vector2.Distance(buddy2.position, buddy3.position);
+        distanceFromBuddy3 = Vector2.Distance(buddy3.position, buddy4.position);
+        distanceFromBuddy4 = Vector2.Distance(buddy4.position, buddy5.position);
+        distanceFromBuddy5 = Vector2.Distance(buddy5.position, buddy6.position);
+        //distanceFromBuddy6 = Vector2.Distance(buddy6.position, buddy2.position);
+
+        
+        if(distanceFromPlayer > 2f) 
+        {
+            buddy1.transform.position = Vector2.MoveTowards(buddy1.position, playerRb.position, step);
+        }
+        else if(distanceFromPlayer < 2f)
+        {
+            buddy1.velocity = Vector2.zero;
+        }
+
+        if (distanceFromBuddy1 > 2f)
+        {
+            buddy2.transform.position = Vector2.MoveTowards(buddy2.position, buddy1.position, step);
+        }
+        else if (distanceFromBuddy1 < 2f)
+        {
+            buddy2.velocity = Vector2.zero;
+        }
+
+        if (distanceFromBuddy2 > 2f)
+        {
+            buddy3.transform.position = Vector2.MoveTowards(buddy3.position, buddy2.position, step);
+        }
+        else if (distanceFromBuddy2 < 2f)
+        {
+            buddy3.velocity = Vector2.zero;
+        }
+        if (distanceFromBuddy3 > 2f)
+        {
+            buddy4.transform.position = Vector2.MoveTowards(buddy4.position, buddy3.position, step);
+        }
+        else if (distanceFromBuddy3 < 2f)
+        {
+            buddy4.velocity = Vector2.zero;
+        }
+        if (distanceFromBuddy4 > 2f)
+        {
+            buddy5.transform.position = Vector2.MoveTowards(buddy5.position, buddy4.position, step);
+        }
+        else if (distanceFromBuddy4 < 2f)
+        {
+            buddy5.velocity = Vector2.zero;
+        }
+        if (distanceFromBuddy5 > 2f)
+        {
+            buddy6.transform.position = Vector2.MoveTowards(buddy6.position, buddy5.position, step);
+        }
+        else if (distanceFromBuddy5 < 2f)
+        {
+            buddy6.velocity = Vector2.zero;
+        }
+
+
+    }
+    void BuddyFollow()
+    {
+        
+
+        
+        
+        
+        
+        
+        
     }
 
     public void OnLanding()
@@ -92,7 +205,7 @@ public class PlayerController : MonoBehaviour
     }
     public void EnemyKill()
     {
-        rb.AddForce(new Vector2(0, bounceForce), ForceMode2D.Impulse );
+        playerRb.AddForce(new Vector2(0, bounceForce), ForceMode2D.Impulse );
 
     }
     
